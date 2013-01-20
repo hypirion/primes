c5b296272d67629031c6114c4916b7485829e966
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-20 23:32:21 +0100

    Implement Clojure wrapper.

--- src/clojure/com/hypirion/primes.clj
+++ src/clojure/com/hypirion/primes.clj
@@ -1,6 +1,15 @@
-(ns com.hypirion.primes)
+(ns com.hypirion.primes
+  (:import [com.hypirion.primes Primes])
+  (:refer-clojure :exclude [take get]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn prime? [n]
+  (Primes/isPrime n))
+
+(defn take [n]
+  (Primes/take n))
+
+(defn get [n]
+  (Primes/get n))
+
+(defn take-below [n]
+  (Primes/takeUnder n))
