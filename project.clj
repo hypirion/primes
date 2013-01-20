b0b727f41eee9e8856a2c9a47cbdd6a174e384ab
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-20 23:01:06 +0100

    Update project.clj to add Java source folder.

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://github.com/hyPiRion/primes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+
+  :warn-on-reflection true
+
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"])
