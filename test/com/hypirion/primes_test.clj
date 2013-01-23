608c5b80481f4f36aaf92b28c4aee15e9a663d76
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-24 00:58:32 +0100

    Add serial take check.

--- test/com/hypirion/primes_test.clj
+++ test/com/hypirion/primes_test.clj
@@ -1,8 +1,15 @@
 (ns com.hypirion.primes-test
   (:use clojure.test
         com.hypirion.pregenerated-primes)
-  (:require [com.hypirion.primes :as p]))
+  (:require [com.hypirion.primes :as p])
+  (:import [com.hypirion.primes Primes]))
 
-(deftest serial-check
+(deftest serial-prime?-check
+  (Primes/clear)
   (doseq [prime hundred-thousand]
     (is (p/prime? prime))))
+
+(deftest serial-take
+  (doall
+   (map #(is (= %1 %2))
+        (p/take 100008) hundred-thousand)))
