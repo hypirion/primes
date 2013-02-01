b6147f090d3d1404d088f3142dbfc430f014c134
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-01 20:58:33 +0100

    Add docstrings to primes, closes #2.

--- src/clojure/com/hypirion/primes.clj
+++ src/clojure/com/hypirion/primes.clj
@@ -2,17 +2,28 @@
   (:import [com.hypirion.primes Primes])
   (:refer-clojure :exclude [take get]))
 
-(defn primes []
+(defn primes
+  "Returns a lazy sequence of all primes in sorted order."
+  []
   (seq Primes/PRIME))
 
-(defn prime? [^long n]
+(defn prime?
+  "Checks whether an integer (long or lower) is a prime. Returns true if yes,
+  false otherwise."
+  [^long n]
   (Primes/isPrime n))
 
-(defn take [n]
+(defn take
+  "Returns the n first primes as an unmodifiable list."
+  [n]
   (Primes/take n))
 
-(defn get [n]
+(defn get
+  "Returns the nth prime number."
+  [n]
   (Primes/get n))
 
-(defn take-below [n]
+(defn take-below
+  "Returns all primes below n as an unmodifiable list."
+  [n]
   (Primes/takeUnder n))
