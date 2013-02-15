675f71037b38b2f8419525a839736a0920b07b10
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-15 22:14:09 +0100

    Implement faster first-above.

--- src/java/com/hypirion/Primes.java
+++ src/java/com/hypirion/Primes.java
@@ -46,16 +46,28 @@ public final class Primes implements Iterable<Integer>{
     }
 
     public static int firstAbove(int v){
-        createAllUnder(v);
-        int i = Collections.binarySearch(primeList, v);
-        if (i < 0)
-            i = ~i;
-        int p = primeList.get(i);
-        if (p <= v){
-            ensureExists(i+1);
-            p = primeList.get(i+1);
+        int p;
+        if (v < primeList.get(size-1)) {
+            int i = Collections.binarySearch(primeList, v);
+            if (i < 0)
+                i = ~i;
+            p = primeList.get(i);
+            if (p <= v){
+                ensureExists(i+1);
+                p = primeList.get(i+1);
+            }
+            return p;
         }
-        return p;
+        else {
+            int from;
+            if ((v & 1) == 1)
+                from = v + 2;
+            else
+                from = v + 1;
+            for (int i = from;; i +=2)
+                if (isPrime(i))
+                    return i;
+        }
     }
 
     public static boolean isPrime(long n){
