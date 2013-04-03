63c91d30043b2d3a059a984319b236596df5a805
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-03 18:25:30 +0200

    Bump Clojure down to 1.2.0.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/hyPiRion/primes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]]
 
   :warn-on-reflection true
 
